1fecfc4bfa8b5139e64b9c1a91248eecd31c7e6b
Stas Kuznetsov <stas@lipsat.ru>
2023-03-30 21:53:23 +0300

    stuffrepo - test

--- internal/repository/staffrepo/staffrepo_test.go
+++ internal/repository/staffrepo/staffrepo_test.go
@@ -100,3 +100,82 @@ func TestRepo_Read(t *testing.T) {
 		t.Fatalf("unexpected error read employee: %v", err)
 	}
 }
+
+func TestRepo_Update(t *testing.T) {
+	r := &Repo{
+		m: make(map[int]domain.Employee),
+	}
+	u := domain.Employee{
+		ID:      123,
+		Name:    "John Doe",
+		PhotoID: 0,
+	}
+	ctx := context.Background()
+
+	// test creating a new employee
+	id, err := r.Create(ctx, u)
+	if err != nil {
+		t.Fatalf("unexpected error creating employee: %v", err)
+	}
+	u.Name = "NEW John Doe"
+	u.PhotoID = 10
+	emp, err := r.Update(ctx, id, u)
+	if err != nil {
+		t.Fatalf("unexpected error update employee: %v", err)
+	}
+	if u.Name != emp.Name {
+		t.Errorf("expected employee name %s, but got %s", u.Name, emp.Name)
+	}
+
+	if !reflect.DeepEqual(emp, u) {
+		t.Errorf("expected employee %v, but got %v", u, emp)
+	}
+}
+
+func TestRepo_ReadAll(t *testing.T) {
+	r := &Repo{
+		m: make(map[int]domain.Employee),
+	}
+	var emploes []domain.Employee
+	u := domain.Employee{
+		ID:      123,
+		Name:    "John Doe",
+		PhotoID: 0,
+	}
+	ctx := context.Background()
+
+	// test creating a new employee
+	_, err := r.Create(ctx, u)
+	if err != nil {
+		t.Fatalf("unexpected error creating employee: %v", err)
+	}
+	emploes = append(emploes, u)
+
+	u.ID = 223
+	u.Name = "Verlom terkov"
+	u.PhotoID = 1
+	_, err = r.Create(ctx, u)
+	if err != nil {
+		t.Fatalf("unexpected error creating employee: %v", err)
+	}
+	emploes = append(emploes, u)
+	u.ID = 2
+	u.Name = "Piter Parker"
+	u.PhotoID = 12
+
+	_, err = r.Create(ctx, u)
+	if err != nil {
+		t.Fatalf("unexpected error creating employee: %v", err)
+	}
+	emploes = append(emploes, u)
+	allemp, err := r.ReadAll(ctx)
+	if err != nil {
+		t.Fatalf("unexpected error read all employee: %v", err)
+	}
+	if !reflect.DeepEqual(emploes, allemp) {
+		t.Errorf("expected employee %v, but got %v", emploes, allemp)
+	}
+	if len(emploes) != len(allemp) {
+		t.Errorf("expected len employee %v, but got len %v", len(emploes), len(allemp))
+	}
+}
