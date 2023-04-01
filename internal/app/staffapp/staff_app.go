01357b57d8f2187fa6ad4354c3278b3ca2cb3d68
Stas Kuznetsov <stas@lipsat.ru>
2023-04-01 10:29:34 +0300

    [001] Structure stuffapp

--- internal/app/staffapp/staff_app.go
+++ internal/app/staffapp/staff_app.go
@@ -23,3 +23,47 @@ func New(repo StaffRepo) *App {
 		repo: repo,
 	}
 }
+
+func (a *App) AddEmployee(ctx context.Context, name string, photoID int) (uint64, error) {
+	employee := domain.Employee{
+		Name:    name,
+		PhotoID: photoID,
+	}
+	id, err := a.repo.Save(ctx, employee)
+	if err != nil {
+		return 0, err
+	}
+	return id, nil
+}
+
+func (a *App) GetEmployee(ctx context.Context, id uint64) (domain.Employee, error) {
+	employee, err := a.repo.Get(ctx, id)
+	if err != nil {
+		return domain.Employee{}, err
+	}
+	return employee, nil
+}
+
+func (a *App) UpdateEmployee(ctx context.Context, employee domain.Employee) (domain.Employee, error) {
+	updateEmployee, err := a.repo.Update(ctx, employee)
+	if err != nil {
+		return domain.Employee{}, err
+	}
+	return updateEmployee, err
+}
+
+func (a *App) DeleteEmployee(ctx context.Context, id uint64) error {
+	err := a.repo.Delete(ctx, id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (a *App) ListEmployee(ctx context.Context) ([]domain.Employee, error) {
+	list, err := a.repo.List(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return list, nil
+}
